e15839c50819878a5847f161f3e3db2fa55c071f
Renato Biancalana da Silva <renato.biancalana.silva@gmail.com>
2013-11-04 23:55:35 -0200

    Inserção do botão na barra

--- src/br/com/isGoodApps/myfirstapp/HomeActivity.java
+++ src/br/com/isGoodApps/myfirstapp/HomeActivity.java
@@ -4,6 +4,7 @@ import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.Menu;
+import android.view.MenuInflater;
 import android.view.View;
 import android.widget.EditText;
 
@@ -20,8 +21,9 @@ public class HomeActivity extends Activity {
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.home, menu);
-        return true;
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.home, menu);
+        return super.onCreateOptionsMenu(menu);
     }
     
     /** Called when the user clicks the Send button */
